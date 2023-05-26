f73615814eb4a0c2578687a95a6546323cc53882
Lerato05 <tebogoseakgoesamukelo@gmail.com>
2023-05-26 10:53:21 +0200

    add link page

---
+++ src/Details.js
@@ -0,0 +1,147 @@
+// Enter all your detials in this file
+// Logo images
+import logogradient from "./assets/logo.svg";
+import logo from "./assets/logo2.svg";
+// Profile Image
+import profile from "./assets/tebogo.jpeg";
+// Tech stack images
+import html from "./assets/techstack/html.png";
+import css from "./assets/techstack/css.png";
+import js from "./assets/techstack/js.png";
+import react from "./assets/techstack/react.png";
+import bootstrap from "./assets/techstack/bootstrap.png";
+import vscode from "./assets/techstack/vscode.png";
+import github from "./assets/techstack/github.png";
+import git from "./assets/techstack/git.png";
+import npm from "./assets/techstack/npm.png";
+
+// Porject Images
+import projectImage1 from "./assets/projects/project1.jpg";
+import projectImage2 from "./assets/projects/project2.jpg";
+import projectImage3 from "./assets/projects/project3.jpg";
+import projectImage4 from "./assets/projects/project4.jpg";
+import projectImage5 from "./assets/projects/project5.jpg";
+import projectImage6 from "./assets/projects/project6.jpg";
+
+// Logos
+export const logos = {
+  logogradient: logogradient,
+  logo: logo,
+};
+
+// Enter your Personal Details here
+export const personalDetails = {
+  name: "Tebogo Seakgoe",
+  tagline: "Web Developer Programmer",
+  img: profile,
+  about: `I am a first-time study Web Developer here in DA for 1 years and I was inspired
+  to learn many things about IT Programming I was Struggling to work hard and teamwork
+  support each other and I improve my skills. I dream to want to become teaching children 
+  how to skill hands with computer IT and we want to empower individuals to get the skills
+  for them.`,
+};
+
+// Enter your Social Media URLs here
+export const socialMediaUrl = {
+  linkdein: "https://www.linkedin.com/",
+  github: "https://www.github.com/",
+  twitter: "https://twitter.com/",
+  instagram: "https://www.instagram.com/",
+};
+
+// Enter your Work Experience here
+export const workDetails = [
+  {
+    Position: "Internship Programmer/ IT Support/ Infystrat /Software",
+    Location: "Sandton",
+    Type: "Full Time",
+    Duration: "June 2019-Current",
+  },
+  {
+    Position: "Assistance Teacher",
+    Location: "Soweto",
+    Type: "Full Time",
+    Duration: "Nov 2021 - March 2022",
+
+              
+  },
+  {
+    Position: "Digital Academy",
+    Location: "Braamfontein",
+    Type: "Full Time",
+    Duration: "June 2022 - May 2023",
+  },
+];
+
+// Enter your Education Details here
+export const eduDetails = [
+  {
+    Position: "Sizwile School for the deaf",
+    Location: "Soweto",
+    Type: "Full Time",
+    Duration: "Jan 2013 - Dec 2013",
+    
+  },
+];
+
+// Tech Stack and Tools
+export const techStackDetails = {
+  html: html,
+  css: css,
+  js: js,
+  react: react,
+  bootstrap: bootstrap,
+  vscode: vscode,
+  npm: npm,
+  git: git,
+  github: github,
+  
+};
+
+// Enter your Project Details here
+export const projectDetails = [
+  {
+    title: "Project title 1",
+    image: projectImage1,
+    description: `This is sample project description random things are here in description This is sample
+project lorem ipsum generator for dummy content`,
+    techstack: "HTML/CSS, JavaScript",
+    previewLink: "https://google.com",
+    githubLink: "https://github.com",
+  },
+  {
+    title: "Project title 2",
+    image: projectImage2,
+    description: `React is a free and open-source front-end JavaScript library for 
+    building user interfaces based on components. `,
+    techstack: "REACT",
+    previewLink: "https://google.com",
+    githubLink: "https://github.com",
+  },
+  {
+    title: "Project title 3",
+    image: projectImage3,
+    description: `Bootstrap is a free, open source front-end development 
+    framework for the creation of websites and web apps.`,
+    techstack: "Bootstrap",
+    previewLink: "https://google.com",
+    githubLink: "https://github.com",
+  },
+  {
+    title: "Project title 4",
+    image: projectImage4,
+    description: `GitHub, Inc. is an Internet hosting service for software development and 
+    version control using Git. It provides the distributed version control of Git plus access control, bug tracking, 
+    software feature requests, task management, continuous integration, and wikis for every project. `,
+    techstack: "Github",
+    previewLink: "https://google.com",
+    githubLink: "https://github.com",
+  },
+  
+];
+
+// Enter your Contact Details here
+export const contactDetails = {
+  email: "tebogoseakgoesamukelo@gmail.com",
+  phone: "083 362 4685",
+};
